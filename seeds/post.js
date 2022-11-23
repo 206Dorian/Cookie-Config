94bfd844cf56d7697f4f132210ebc2f1e728797b
Dorian Birch <206dorian@gmail.com>
2022-11-23 17:26:40 -0600

    added data to seeds files, used fictional literally characters

---
+++ seeds/post.js
@@ -0,0 +1,18 @@
+[
+    {
+      "postbody": "I Love Artwork!",
+      "user_id": 1,
+      
+    },
+    {
+      "postbody": "I have a secret love!",
+      "user_id": 2,
+      
+    },
+    {
+      "postbody": "I Love to Party everynight!",
+      "user_id": 3,
+      
+    },
+  ]
+  
---
+++ seeds/user.js
@@ -0,0 +1,17 @@
+[
+  {
+    "name": "Dorian Grey",
+    "email": "Dgrey@aol.com",
+    "password": 'DontLookInTheAttic'
+  },
+  {
+    "name": "Mr Darcy",
+    "email": "Fritzdarcy@gmail.com",
+    "password": 'JanePride1813'
+  },
+  {
+    "name": "Jay Gatsby",
+    "email": "GreatestGatsby@hotmail.com",
+    "password": 'Bootlegger#1'
+  }
+]
